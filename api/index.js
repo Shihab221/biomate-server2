49a446c1b830bb77af04da0c409ccd396bd1466c
Shihab Ahemed <64270227+Shihab221@users.noreply.github.com>
2024-10-04 12:36:20 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,14 +3,15 @@ import mongoose from "mongoose";
 import config from "../src/config/config";
 import app from "../src/app";
 import cors from "cors";
-const corsOptions = {
-  origin: 'http://localhost:3000',  // Set this to your frontend's URL
-  methods: ['GET', 'POST'],          // List allowed methods, or allow all
-  allowedHeaders: ['Content-Type'],  // Specify allowed headers if needed
-  credentials: true                  // Allow credentials if needed
-};
+// const corsOptions = {
+//   origin: 'http://localhost:3000',  // Set this to your frontend's URL
+//   methods: ['GET', 'POST'],          // List allowed methods, or allow all
+//   allowedHeaders: ['Content-Type'],  // Specify allowed headers if needed
+//   credentials: true                  // Allow credentials if needed
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors({ origin: '*' }));
 
 app.listen(config.port, (err) => {
   if (err) console.log(err);
