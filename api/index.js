c577ea8a9f7c6e0e563c64cd1d6f062c3be07ce9
Shihab Ahemed <64270227+Shihab221@users.noreply.github.com>
2024-10-04 15:34:40 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,21 +3,17 @@ import mongoose from "mongoose";
 import config from "../src/config/config";
 import app from "../src/app";
 import cors from "cors";
-const corsOptions = {
-  // origin: 'http://localhost:3000', // Replace with your frontend URL
-  methods: ['GET', 'POST'], // Specify allowed methods if needed
-  allowedHeaders: ['Content-Type', 'Authorization'], // Include headers you need to allow
-  credentials: true, // This allows credentials (cookies, authorization headers) to be sent
-};
+// const corsOptions = {
+//   // origin: 'http://localhost:3000', // Replace with your frontend URL
+//   methods: ['GET', 'POST'], // Specify allowed methods if needed
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Include headers you need to allow
+//   credentials: true, // This allows credentials (cookies, authorization headers) to be sent
+// };
 
 // Apply the CORS middleware with options
-app.use(cors({ origin: '*' },corsOptions));
+app.use(cors({ origin: '*' }));
 
-// app.options('*', cors(corsOptions)); // Allow preflight requests for all routes
 
-// { origin: '*' }
-// app.use(cors(corsOptions));
-// app.use(cors());
 
 app.listen(config.port, (err) => {
   if (err) console.log(err);
