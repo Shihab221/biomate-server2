cce002fa111ec550dcf354fb490b2ea627c37aec
Shihab Ahemed <64270227+Shihab221@users.noreply.github.com>
2024-10-04 15:28:16 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,6 +13,8 @@ const corsOptions = {
 // Apply the CORS middleware with options
 app.use(cors(corsOptions));
 
+app.options('*', cors(corsOptions)); // Allow preflight requests for all routes
+
 // { origin: '*' }
 // app.use(cors(corsOptions));
 // app.use(cors());
