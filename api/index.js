8a2a6b50096ebf6313b25d9cc5274bbfdaebeb8f
Shihab Ahemed <64270227+Shihab221@users.noreply.github.com>
2024-10-05 22:57:30 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-/* eslint-disable no-console */
-const mongoose = require("mongoose");
-const config = require("../src/config/config");
-const app = require("../src/app");
-const cors = require("cors");
 
-// Update CORS options to allow all origins
+/* eslint-disable no-console */
+import mongoose from "mongoose";
+import config from "../src/config/config";
+import app from "../src/app";
+import cors from "cors";
+
 const corsOptions = {
-  origin: '*', // Allow all origins
+  origin: 'http://localhost:3001/', // Replace with your actual frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // List the HTTP methods you want to allow
   credentials: true, // This allows cookies to be sent
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Specify the headers allowed
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
 
 app.options('*', cors(corsOptions)); // This enables handling preflight requests
 
+
 app.listen(config.port, (err) => {
   if (err) console.log(err);
   console.log(`Server started at port ${config.port}`);
@@ -35,42 +36,3 @@ mongoose
   .connect(config.mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected..."))
   .catch((e) => console.log(e));
-
-
-// /* eslint-disable no-console */
-// import mongoose from "mongoose";
-// import config from "../src/config/config";
-// import app from "../src/app";
-// import cors from "cors";
-
-// const corsOptions = {
-//   origin: 'http://localhost:3001/', // Replace with your actual frontend domain
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // List the HTTP methods you want to allow
-//   credentials: true, // This allows cookies to be sent
-//   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Specify the headers allowed
-// };
-
-// // Apply the CORS middleware with options
-// app.use(cors(corsOptions));
-
-// app.use((req, res, next) => {
-//   res.cookie('cookieName', 'cookieValue', { 
-//     sameSite: 'None', 
-//     secure: true 
-//   });
-//   next();
-// });
-
-// app.options('*', cors(corsOptions)); // This enables handling preflight requests
-
-
-// app.listen(config.port, (err) => {
-//   if (err) console.log(err);
-//   console.log(`Server started at port ${config.port}`);
-// });
-
-// mongoose.Promise = global.Promise;
-// mongoose
-//   .connect(config.mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("MongoDB successfully connected..."))
-//   .catch((e) => console.log(e));
