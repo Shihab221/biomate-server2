bed1661b7850a722f8fd4cde9614fa6b3039347a
Shihab Ahemed <64270227+Shihab221@users.noreply.github.com>
2024-10-05 23:17:58 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import app from "../src/app";
 import cors from "cors";
 
 const corsOptions = {
-  origin: 'http://localhost:3002', // Replace with your actual frontend domain
+  origin: '*', // Replace with your actual frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // List the HTTP methods you want to allow
   credentials: true, // This allows cookies to be sent
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Specify the headers allowed
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.options('*', cors(corsOptions)); // This enables handling preflight requests
+app.options('*', cors(corsOptions)); // This enables handling preflight requests
 
 
 app.listen(config.port, (err) => {
